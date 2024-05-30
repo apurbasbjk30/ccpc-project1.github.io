717275ee6364619bff93f9087902e8e392688a4c
Krish Kumar <122594525+Imkkrish@users.noreply.github.com>
2024-05-30 22:04:37 +0530

    Update script.js
    added function for dotted circle for animation

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ window.addEventListener('scroll', function() {
     const navButtons = document.querySelector('.navbar');
     const outputDiv = document.getElementById('output');
     const wordDiv = document.getElementById('word');
+
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
     if (scrollTop < 200) {
@@ -122,4 +123,75 @@ document.addEventListener('click', function(event) {
         nav.style.display = "none";
         bars.classList.remove("change");
     }
+});
+
+//mouse hover on search box
+document.addEventListener('DOMContentLoaded', () => {
+  const searchBox = document.querySelector('.searchBox');
+  const searchInput = document.querySelector('.searchInput');
+
+  searchBox.addEventListener('mouseover', () => {
+      searchInput.focus();
+  });
+
+  searchBox.addEventListener('mouseout', () => {
+      searchInput.blur();
+  });
+});
+
+
+//for circles 
+window.addEventListener('scroll', () => {
+  const scrollPosition = window.scrollY;
+  const windowHeight = window.innerHeight;
+
+  const circle1 = document.querySelector('.circle1');
+  const circle2 = document.querySelector('.circle2');
+  const circle3 = document.querySelector('.circle3');
+  const circle4 = document.querySelector('.circle4');
+
+  const start = windowHeight / 2; // Start animation when scrolled halfway down the viewport
+  const end = windowHeight; // End animation when the viewport height is reached
+
+  if (scrollPosition >= start && scrollPosition <= end) {
+    const progress = (scrollPosition - start) / (end - start);
+
+    circle3.style.left = `${progress * 99}%`;
+    circle3.style.top = `${65 - (65 - 38) * progress}%`;
+
+    circle2.style.left = `${progress * 85}%`;
+    circle2.style.top = `${50 - 50 * progress}%`;
+
+    circle1.style.left = `${progress * 99}%`;
+    circle1.style.top = `${9 + (50 - 23) * progress}%`;
+
+    circle4.style.left = `${progress * 85}%`;
+    circle4.style.top = `${45 - (45 - 45) * progress}%`;
+  } else if (scrollPosition < start) {
+    // Reset positions if above start scroll position
+    circle1.style.left = `0%`;
+    circle1.style.top = `50%`;
+
+    circle2.style.left = `0%`;
+    circle2.style.top = `50%`;
+
+    circle3.style.left = `0%`;
+    circle3.style.top = `50%`;
+
+    circle4.style.left = `0%`;
+    circle4.style.top = `50%`;
+  } else {
+    // Fix positions if below end scroll position
+    circle1.style.left = `98%`;
+    circle1.style.top = `36%`;
+
+    circle2.style.left = `85%`;
+    circle2.style.top = `0%`;
+
+    circle3.style.left = `99%`;
+    circle3.style.top = `38%`;
+
+    circle4.style.left = `84%`;
+    circle4.style.top = `45%`;
+  }
 });
